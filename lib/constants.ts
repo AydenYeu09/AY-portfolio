a7584b042594d026db27619ea812530fed7b3c48
Scorchify <139656419+Scorchify@users.noreply.github.com>
2026-02-18 00:18:24 -0500

    quick fixes pt 2

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-	name: 'Engineering Portfolio',
+	name: 'Ayden Yeung - Engineering Portfolio',
 	description:
 		'A professional portfolio website template for engineering students.',
 	mainNav: [
